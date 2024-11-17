2738ff53b034e0ef6148d79ed8347de9375e800c
RondLeSoleil <saintpierregerald01@gmail.com>
2024-11-17 05:54:45 +0100

    Add files via upload

---
+++ Toast_Notification/script.js
@@ -0,0 +1,18 @@
+let toastBox = document.getElementById('toastBox');
+let successMsg = "Success";
+let errorMsg = "Error";
+let invalidMsg = "Invalid";
+
+function showToast(msg,classToast="toast") {
+    // Créer l'élément du toast
+    let toast = document.createElement('div');
+    toast.classList.add(classToast); // Classe pour le style
+    toast.innerHTML = msg;
+
+    // Ajouter le toast au conteneur
+    toastBox.appendChild(toast);
+    // Supprimer le toast après un délai
+    setTimeout(() => {
+        toast.remove();
+    }, 3000);
+}
